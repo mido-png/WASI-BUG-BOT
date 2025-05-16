fa1d120ad34df4d65033866b8e9216df7222777b
mido-png <momanyibrian627@gmail.com>
2025-05-16 10:03:26 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,15 +3,15 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "254729706456"
+global.ownername = "🐛 MR shêrrîf "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
 
-global.ownernumber = '923347748038'  //creator number
-global.ownername = ' MR WASI ' //owner name
-global.botname = 'WASI TECH BUG BOT ' //name of the bot
+global.ownernumber = '254729706456'  //creator number
+global.ownername = ' MR shêrrîf ' //owner name
+global.botname = 'SHERRIF TECH BUG BOT ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
